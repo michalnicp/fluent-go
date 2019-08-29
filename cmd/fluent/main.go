9bd3985ceb319f3cf1afb8ce2896f66d32617327
Michal Nicpon <michalnicp@gmail.com>
2019-08-28 22:52:02 -0600

    Add cli tool for parsing fluent syntax files

---
+++ cmd/fluent/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
+
+	"github.com/michalnicp/fluent-go/syntax"
+)
+
+var version = "0.0.0"
+
+var usage = `Usage: fluent [options] [file]...
+
+Options:
+  -h, -help      Print this message and exit.
+  -v, -version   Print the version and exit.`
+
+func main() {
+	var code int
+	defer func() { os.Exit(code) }()
+
+	var (
+		helpRequested    bool
+		versionRequested bool
+	)
+
+	flag.BoolVar(&helpRequested, "help", false, "")
+	flag.BoolVar(&helpRequested, "h", false, "")
+	flag.BoolVar(&versionRequested, "version", false, "")
+	flag.BoolVar(&versionRequested, "v", false, "")
+	flag.Parse()
+
+	if helpRequested {
+		fmt.Println(usage)
+		return
+	}
+
+	if versionRequested {
+		fmt.Println(version)
+		return
+	}
+
+	if len(flag.Args()) == 0 {
+		fmt.Println(usage)
+		code = 1
+		return
+	}
+
+	for _, file := range flag.Args() {
+		input, err := ioutil.ReadFile(file)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "read %s: %v\n", file, err)
+			code = 1
+		}
+
+		if _, err := syntax.Parse(input); err != nil {
+			fmt.Fprintf(os.Stderr, "parse %s:\n%+v\n", file, err)
+			code = 1
+		}
+	}
+	os.Exit(code)
+}
--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module github.com/michalnicp/fluent-go
 go 1.12
 
 require (
-	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/stretchr/testify v1.4.0
+	golang.org/x/text v0.3.2
 )
